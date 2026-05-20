90eea4f75f163621ff1a1ad5970325ccbe4d9933
proton96 <josecarlosmartinlopez10@gmail.com>
2026-05-20 13:04:35 +0200

    Seguimos con estructura

---
+++ employee-portal-frontend/src/app/shared/components/layout/layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css'
+})
+export class LayoutComponent {}
