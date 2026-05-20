2028d2b417d392b277db399fe3517e4f85371b74
Jay Roldán Ruiz <animaloser@gmail.com>
2026-05-20 11:03:40 +0200

    añadido la base de spring

---
+++ backend-spring/src/main/java/com/glitchhunters/portalempresa/PortalempresaApplication.java
@@ -0,0 +1,13 @@
+package com.glitchhunters.portalempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalempresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalempresaApplication.class, args);
+	}
+
+}
